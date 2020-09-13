e83cffa438d1e479fd45f5de609c2776c31870f4
Aleksandar Misljenovic <a.misljenovic@accruentatlas.onmicrosoft.com>
2020-09-13 19:05:43 +0200

    minor fixes

--- src/reducers/hallReducer.ts
+++ src/reducers/hallReducer.ts
@@ -1,15 +1,10 @@
 import { GET_HALL } from 'actions/types';
-import Hall from 'models/hall.model';
 
 const initalState = {
   hall: null,
 };
 
-type HallState = {
-  hall: Hall | null;
-}
-
-export default (state: HallState = initalState, action) => {
+export default (state = initalState, action) => {
   switch (action.type) {
     case GET_HALL:
       return {
